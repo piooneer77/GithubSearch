3e3619f4b98d7e3d6e02759cd8d3597e12479eee
Karem Gohar <piooneer77@me.com>
2020-07-20 14:13:50 +0200

    init

---
+++ GithubSearch/GithubSearch/GithubSearch.h
@@ -0,0 +1,19 @@
+//
+//  GithubSearch.h
+//  GithubSearch
+//
+//  Created by Karem Gohar on 20.07.20.
+//  Copyright © 2020 Karem Gohar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GithubSearch.
+FOUNDATION_EXPORT double GithubSearchVersionNumber;
+
+//! Project version string for GithubSearch.
+FOUNDATION_EXPORT const unsigned char GithubSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubSearch/PublicHeader.h>
+
+
